576e7643dd28193a3853acc05ed64b6e94ff8df9
Alain Beauvois <alain@itipic.com>
2014-12-03 21:57:01 +0100

    Add click listener on button

--- app/src/main/java/com/questioncode/myschool/LoginActivity.java
+++ app/src/main/java/com/questioncode/myschool/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends Activity implements View.OnClickListener {
@@ -13,6 +15,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.act_login_submit_btn).setOnClickListener(this);
     }
 
 
